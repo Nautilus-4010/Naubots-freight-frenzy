fb14b79cef049511c3c1c2ed7d4b17fca0830579
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-11-04 21:17:20 -0600

    Included TFOD

--- teamcode/robot/FTCRobot.java
+++ teamcode/robot/FTCRobot.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.robot;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.utils.TargetInfo;
+import java.util.List;
 
 
 public class FTCRobot {
@@ -20,7 +22,7 @@ public class FTCRobot {
     
     public void initializeMechanisms(){
         HardwareMap hwMap = programa.hardwareMap;
-        chasis.initializeHardware(hwMap);
+        //chasis.initializeHardware(hwMap);
         vision.initializeHardware(hwMap);
         programa.telemetry.addData("Status", "Ready to rumbleee!!!");
     }
@@ -31,5 +33,12 @@ public class FTCRobot {
             programa.telemetry.addData("Identified target", identifiedTarget.name);
             programa.telemetry.addData("Pos (mm)", "{X, Y, Z} = %.1f, %.1f, %.1f", identifiedTarget.x, identifiedTarget.y, identifiedTarget.z);
         }
+        List<Recognition> identifiedObjects = vision.getRecognizedObjects();
+        if(identifiedObjects != null){
+            programa.telemetry.addData("Found objects", identifiedObjects.size());
+            for(Recognition object: identifiedObjects)
+                programa.telemetry.addData("Objeto", object.getLabel());
+        }
+            
     }
 }
