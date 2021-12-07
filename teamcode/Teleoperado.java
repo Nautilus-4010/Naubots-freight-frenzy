ce5ef65791b04bd1daf81b6e0233e6a5c376b673
Ernesto Miranda <55798237+ErnoMitrovic@users.noreply.github.com>
2021-12-06 19:14:57 -0600

    dropServo agregado
    @ULISES008 agregar los métodos del celular #6

--- teamcode/Teleoperado.java
+++ teamcode/Teleoperado.java
@@ -51,9 +51,11 @@ public class Teleoperado extends OpMode{
             robot.pickFreight();
         else if(gamepad1.right_trigger > 0.5)
             robot.dropFreight();
+        else if(gamepad1.dpad_dow)
+            robot.dropServo();
         else
             robot.stopInTake();
-            
+
         //SuperPato control
         if(gamepad2.a){
             robot.dropSuperPato();
