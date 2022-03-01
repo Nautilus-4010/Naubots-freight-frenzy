7515b38fe5b2ca98f07f2973410af73cfc387474
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-02-28 22:01:42 -0600

    Código final Toluca 2022 (#8)

--- teamcode/Teleoperado.java
+++ teamcode/Teleoperado.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Naubot;
 
-
 @TeleOp(name="Teleoperado")
+
 public class Teleoperado extends OpMode{
     private Naubot robot;
 
@@ -26,63 +27,72 @@ public class Teleoperado extends OpMode{
         
     }
 
-//////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
     @Override
     public void loop(){
-        double powerMultiplier; 
-        double drive = -gamepad1.left_stick_y;
-        double lateral = gamepad1.left_stick_x;
-        double turn = gamepad1.right_stick_x;
+
+    // Chasis control
+    moveChasis(gamepad1);
         
-        if(gamepad1.right_trigger > 0.5)
-            powerMultiplier = 1.0;
-        else
-            powerMultiplier = 0.4;
-        if(gamepad1.dpad_up)
-            drive = 0.8;
-        else if(gamepad1.dpad_down)
-            drive = -0.8;
-        if(gamepad1.dpad_left)
-            lateral = -0.8;
-        else if(gamepad1.dpad_right)
-            lateral = 0.8;
-        robot.move(drive, lateral, turn, powerMultiplier);
         
-        // Intake control
-        
-        if(gamepad2.left_trigger > 0.5)
-            robot.pickBackFreight();
-        else if(gamepad2.right_trigger > 0.5)
-            robot.pickFrontFreight(); 
-        else if(gamepad2.dpad_down)
+    // Intake control
+        if(gamepad2.left_trigger > 0 && gamepad2.right_trigger > 0)
+            robot.exitFreight();
+        else if(gamepad2.left_trigger > 0)
             robot.dropFreight();
+        else if(gamepad2.right_trigger > 0 || gamepad1.right_trigger > 0)
+            robot.pickFreight();
         else
             robot.stopInTake();
+        
+        //(gamepad1.left_trigger > 0)
+           // robot.dropFreight();
 
-        //SuperPato control
+    //SuperPato control
         
-        if(gamepad2.x){
+        if(gamepad2.dpad_right)
             robot.dropBlueSuperPato();
-        } else if(gamepad2.b){
+        else if(gamepad2.dpad_left)
             robot.dropRedSuperPato();
-        }else {
+        else if(gamepad2.dpad_down)
+            robot.dropBlueSuperPatoRemix();
+        else if(gamepad2.dpad_up) 
+            robot.dropRedSuperPatoRemix();
+        else
             robot.stopSuperPato();
-        }
-        
-        // Cuatro barras control
+      
+    // Cuatro barras control
         
         controlCuatroBarras();
         logRobotStatus();
+        
+    // Brazo control
+    
+        //double brazoPower = gamepad2.left_stick_x;
+        //robot.controlBrazo(brazoPower); 
+        
+        if(gamepad1.dpad_up)
+            robot.brazoGuardado();
+        else if(gamepad1.dpad_left)
+            robot.brazoPick();
+        else if(gamepad1.dpad_down)
+            robot.brazoDrop();
+    
+    
+    
     }
     
-////////////////////////////////////////////////
+    
+    
+
+/////////////////////////////////////////////////////////////
 
     @Override
     public void stop(){}
     
     private void logRobotStatus(){
-        telemetry.addData("Servo: ", robot.servo.getPosition());
+        telemetry.addData("Servo Caja: ", robot.caja.getPosition());
         telemetry.addData("POWER", "");
         telemetry.addData("", String.format("%.2f | %.2f", robot.frontLeft.getPower(), robot.frontRight.getPower()));
         telemetry.addData("", String.format("%.2f | %.2f", robot.backLeft.getPower(), robot.backRight.getPower()));
@@ -93,19 +103,21 @@ public class Teleoperado extends OpMode{
         telemetry.addData("Back left", "" + robot.backLeft.getCurrentPosition());
         telemetry.addData("Back right", "" + robot.backRight.getCurrentPosition());
         
-        telemetry.addData("Posicion potenciometro: %.3f", robot.pot.getPotPosition());
-        
         telemetry.update();
     }
 
+/////////////////////////////////////////////////////////////
+
     private void controlCuatroBarras(){
-        double motor4bPower = 0.8;
-        if(gamepad2.a) { 
-            robot.cuatroBarras.setPower(motor4bPower);
-        }else if(gamepad2.y){
-            robot.cuatroBarras.setPower(-motor4bPower);
-        }else{
-            robot.cuatroBarras.setPower(0);
-        }
+        double motor4bPower = gamepad2.right_stick_y;
+        robot.cuatroBarras.setPower(motor4bPower/2);
+    }
+    
+    private void moveChasis(Gamepad gamepad) {
+        double powerMultiplier = 1;
+        double drive = -gamepad.left_stick_y;
+        double lateral = gamepad.left_stick_x;
+        double turn = gamepad.right_stick_x;
+        robot.move(drive, lateral, turn, powerMultiplier);
     }
 }
